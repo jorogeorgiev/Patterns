aa2bf0fe8741defec6d3bd924117d5024921f02f
Georgi Georgiev <georgi.hristov@clouway.com>
2012-10-08 10:28:01 +0300

    Observer notifies collaborator about change of a product.

--- test/com/clouway/observer/ObserverTest.java
+++ test/com/clouway/observer/ObserverTest.java
@@ -20,8 +20,6 @@ public class ObserverTest {
 
   private Product apples;
 
-
-
   @Before
   public void setUp() {
 
@@ -29,20 +27,19 @@ public class ObserverTest {
 
     observer = mock(StockObserver.class);
 
-    store.addObserver(observer);
+    store.attachObserver(observer);
 
     apples = mock(Product.class);
 
 
   }
 
-
   private class Store {
 
     List<StockObserver> observers = Lists.newArrayList();
 
 
-    public void addObserver(StockObserver observer) {
+    public void attachObserver(StockObserver observer) {
 
       observers.add(observer);
 
@@ -81,19 +78,47 @@ public class ObserverTest {
     void notifyAbout(Product product);
 
 
+    void addPartner(Partner shopPartner);
+
   }
+
+  class MetroStockObserver implements StockObserver {
+
+    private List<Partner> partners = Lists.newArrayList();
+
+    @Override
+    public void notifyAbout(Product product) {
+
+      for (Partner partner : partners) {
+
+        partner.notifyAbout(product);
+
+      }
+
+    }
+
+    @Override
+    public void addPartner(Partner partner) {
+
+      partners.add(partner);
+
+    }
+  }
+
 
   interface Product {
 
-    void setAvailability(Boolean availability);
-
-    Boolean isAvailable();
-
   }
 
 
+  interface Partner {
+
+    void notifyAbout(Product product);
+
+  }
+
   @Test
-  public void storeUpdatesObserversWhenNewProductAdded() {
+  public void storeNotifiesObserversWhenNewProductAdded() {
 
     store.addProduct(apples);
 
@@ -103,11 +128,26 @@ public class ObserverTest {
 
 
   @Test
-  public void storeUpdatesObserversWhenProductSold() {
+  public void storeNotifiesObserversWhenProductSold() {
 
     store.sellProduct(apples);
 
     verify(observer).notifyAbout(apples);
+
+  }
+
+  @Test
+  public void observerNotifiesPartnersAboutProductChange() {
+
+    Partner shopPartner = mock(Partner.class);
+
+    MetroStockObserver metroObserver = new MetroStockObserver();
+
+    metroObserver.addPartner(shopPartner);
+
+    metroObserver.notifyAbout(apples);
+
+    verify(shopPartner).notifyAbout(apples);
 
   }
 
